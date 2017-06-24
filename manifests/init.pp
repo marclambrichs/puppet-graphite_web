def4f6b4272b133f43a6d56faeb61c53c0b08b76
Bram Vogelaar <attachmentgenie@inuits.eu>
2017-06-24 20:11:43 +0200

    Enable the option to cache queries in memcache

--- manifests/init.pp
+++ manifests/init.pp
@@ -34,6 +34,9 @@
 # @param graphite_web_pkgs
 # @param group
 # @param manage_pkg
+# @param memcached_python_ensure
+# @param memcached_python_pkgs
+# @param memcached_enabled
 # @param user
 #
 # === Authors
@@ -45,37 +48,42 @@
 # Copyright 2017 Marc Lambrichs, unless otherwise noted
 #
 class graphite_web (
-  $carbon_caches                         = $::graphite_web::params::carbon_caches,
-  $config_auto_refresh_interval          = $::graphite_web::params::config_auto_refresh_interval,
-  $config_carbon_metric_prefix           = $::graphite_web::params::config_carbon_metric_prefix,
-  $config_carbonlink_hashing_type        = $::graphite_web::params::config_carbonlink_hashing_type,
-  $config_carbonlink_retry_delay         = $::graphite_web::params::config_carbonlink_retry_delay,
-  $config_carbonlink_timeout             = $::graphite_web::params::config_carbonlink_timeout,
-  $config_database_engine                = $::graphite_web::params::config_database_engine,
-  $config_databases                      = $::graphite_web::params::config_databases,
-  $config_date_format                    = $::graphite_web::params::config_date_format,
-  $config_debug                          = $::graphite_web::params::config_debug,
-  $config_dir                            = $::graphite_web::params::config_dir,
-  $config_graphite_root                  = $::graphite_web::params::config_graphite_root,
-  $config_index_file                     = $::graphite_web::params::config_index_file,
-  $config_log_cache_performance          = $::graphite_web::params::config_log_cache_performance,
-  $config_log_dir                        = $::graphite_web::params::config_log_dir,
-  $config_log_rendering_performance      = $::graphite_web::params::config_log_rendering_performance,
-  $config_log_rotation                   = $::graphite_web::params::config_log_rotation,
-  $config_log_rotation_count             = $::graphite_web::params::config_log_rotation_count,
-  $config_max_tag_length                 = $::graphite_web::params::config_max_tag_length,
-  $config_replication_factor             = $::graphite_web::params::config_replication_factor,
-  $config_secret_key                     = $::graphite_web::params::config_secret_key,
-  $config_storage_dir                    = $::graphite_web::params::config_storage_dir,
-  $config_time_zone                      = $::graphite_web::params::config_time_zone,
-  $config_url_prefix                     = $::graphite_web::params::config_url_prefix,
-  $config_use_remote_user_authentication = $::graphite_web::params::config_use_remote_user_authentication,
-  $config_whisper_dir                    = $::graphite_web::params::config_whisper_dir,
-  $graphite_web_ensure                   = $::graphite_web::params::graphite_web_ensure,
-  $graphite_web_pkgs                     = $::graphite_web::params::graphite_web_pkgs,
-  $group                                 = $::graphite_web::params::group,
-  $manage_pkg                            = $::graphite_web::params::manage_pkg,
-  $user                                  = $::graphite_web::params::user,
+  $carbon_caches                           = $::graphite_web::params::carbon_caches,
+  $config_auto_refresh_interval            = $::graphite_web::params::config_auto_refresh_interval,
+  $config_carbon_metric_prefix             = $::graphite_web::params::config_carbon_metric_prefix,
+  $config_carbonlink_hashing_type          = $::graphite_web::params::config_carbonlink_hashing_type,
+  $config_carbonlink_retry_delay           = $::graphite_web::params::config_carbonlink_retry_delay,
+  $config_carbonlink_timeout               = $::graphite_web::params::config_carbonlink_timeout,
+  $config_database_engine                  = $::graphite_web::params::config_database_engine,
+  $config_databases                        = $::graphite_web::params::config_databases,
+  $config_date_format                      = $::graphite_web::params::config_date_format,
+  $config_debug                            = $::graphite_web::params::config_debug,
+  $config_dir                              = $::graphite_web::params::config_dir,
+  $config_graphite_root                    = $::graphite_web::params::config_graphite_root,
+  $config_index_file                       = $::graphite_web::params::config_index_file,
+  $config_log_cache_performance            = $::graphite_web::params::config_log_cache_performance,
+  $config_log_dir                          = $::graphite_web::params::config_log_dir,
+  $config_log_rendering_performance        = $::graphite_web::params::config_log_rendering_performance,
+  $config_log_rotation                     = $::graphite_web::params::config_log_rotation,
+  $config_log_rotation_count               = $::graphite_web::params::config_log_rotation_count,
+  $config_max_tag_length                   = $::graphite_web::params::config_max_tag_length,
+  $config_memcached_default_cache_duration = $::graphite_web::params::config_memcached_default_cache_duration,
+  $config_memcached_hosts                  = $::graphite_web::params::config_memcached_hosts,
+  $config_replication_factor               = $::graphite_web::params::config_replication_factor,
+  $config_secret_key                       = $::graphite_web::params::config_secret_key,
+  $config_storage_dir                      = $::graphite_web::params::config_storage_dir,
+  $config_time_zone                        = $::graphite_web::params::config_time_zone,
+  $config_url_prefix                       = $::graphite_web::params::config_url_prefix,
+  $config_use_remote_user_authentication   = $::graphite_web::params::config_use_remote_user_authentication,
+  $config_whisper_dir                      = $::graphite_web::params::config_whisper_dir,
+  $graphite_web_ensure                     = $::graphite_web::params::graphite_web_ensure,
+  $graphite_web_pkgs                       = $::graphite_web::params::graphite_web_pkgs,
+  $group                                   = $::graphite_web::params::group,
+  $manage_pkg                              = $::graphite_web::params::manage_pkg,
+  $memcached_python_ensure                 = $::graphite_web::params::memcached_python_ensure,
+  $memcached_python_pkgs                   = $::graphite_web::params::memcached_python_pkgs,
+  $memcached_enabled                       = $::graphite_web::params::memcached_enabled,
+  $user                                    = $::graphite_web::params::user,
 ) inherits graphite_web::params {
 
   member( ['carbon_ch', 'fnv1a_ch'], $config_carbonlink_hashing_type )
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,16 +1,16 @@
 class graphite_web::params {
-  $carbon_caches                          = { a =>
+  $carbon_caches                           = { a =>
     {
       cache_query_port => 7102
     }
   }
-  $config_auto_refresh_interval           = 60
-  $config_carbon_metric_prefix            = 'carbon'
-  $config_carbonlink_hashing_type         = 'carbon_ch'
-  $config_carbonlink_retry_delay          = 15
-  $config_carbonlink_timeout              = '1.0'
-  $config_database_engine                 = 'sqlite3'
-  $config_databases                       = { 'default' =>
+  $config_auto_refresh_interval            = 60
+  $config_carbon_metric_prefix             = 'carbon'
+  $config_carbonlink_hashing_type          = 'carbon_ch'
+  $config_carbonlink_retry_delay           = 15
+  $config_carbonlink_timeout               = '1.0'
+  $config_database_engine                  = 'sqlite3'
+  $config_databases                        = { 'default' =>
     {
       name     => '/var/lib/graphite-web/storage/graphite.db',
       engine   => 'django.db.backends.sqlite3',
@@ -20,27 +20,32 @@ class graphite_web::params {
       user     => '',
     }
   }
-  $config_date_format                     = '%m/%d'
-  $config_debug                           = 'False'
-  $config_dir                             = '/etc/graphite-web'
-  $config_graphite_root                   = '/var/lib/graphite-web'
-  $config_index_file                      = '/var/lib/graphite-web/webapp/index'
-  $config_log_cache_performance           = 'True'
-  $config_log_dir                         = '/var/log/graphite-web'
-  $config_log_rendering_performance       = 'True'
-  $config_log_rotation                    = 'True'
-  $config_log_rotation_count              = 1
-  $config_max_tag_length                  = 50
-  $config_replication_factor              = 1
-  $config_secret_key                      = 'secret'
-  $config_storage_dir                     = '/var/lib/graphite-web/storage'
-  $config_time_zone                       = 'Europe/Amsterdam'
-  $config_url_prefix                      = '/graphite'
-  $config_use_remote_user_authentication  = 'False'
-  $config_whisper_dir                     = '/var/lib/carbon/whisper'
-  $graphite_web_ensure                    = present
-  $graphite_web_pkgs                      = [ 'graphite-web',  ]
-  $group                                  = 'graphite-web'
-  $manage_pkg                             = true
-  $user                                   = 'graphite-web'
+  $config_date_format                      = '%m/%d'
+  $config_debug                            = 'False'
+  $config_dir                              = '/etc/graphite-web'
+  $config_graphite_root                    = '/var/lib/graphite-web'
+  $config_index_file                       = '/var/lib/graphite-web/webapp/index'
+  $config_log_cache_performance            = 'True'
+  $config_log_dir                          = '/var/log/graphite-web'
+  $config_log_rendering_performance        = 'True'
+  $config_log_rotation                     = 'True'
+  $config_log_rotation_count               = 1
+  $config_max_tag_length                   = 50
+  $config_replication_factor               = 1
+  $config_secret_key                       = 'secret'
+  $config_storage_dir                      = '/var/lib/graphite-web/storage'
+  $config_time_zone                        = 'Europe/Amsterdam'
+  $config_url_prefix                       = '/graphite'
+  $config_use_remote_user_authentication   = 'False'
+  $config_whisper_dir                      = '/var/lib/carbon/whisper'
+  $graphite_web_ensure                     = present
+  $graphite_web_pkgs                       = [ 'graphite-web',  ]
+  $group                                   = 'graphite-web'
+  $manage_pkg                              = true
+  $config_memcached_default_cache_duration = 60
+  $config_memcached_hosts                  = ['127.0.0.1:11211']
+  $memcached_python_ensure                 = present
+  $memcached_python_pkgs                   = [ 'python-memcached', ]
+  $memcached_enabled                       = false
+  $user                                    = 'graphite-web'
 }
